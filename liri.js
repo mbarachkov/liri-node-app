55ac91a35801eed2032a23820513c5aa7891409c
Michael Barachkov <mike.barachkov@outlook.com>
2019-06-19 20:30:34 -0700

    spotify search with song name works now, reverts for movie-this and spotify this do not work

--- liri.js
+++ liri.js
@@ -1,112 +1,91 @@
-require("dotenv").config();
-// var spotify = new Spotify(keys.spotify);
-// console.log(spotify)
-var moment = require("moment");
-var axios = require("axios");
 // `concert-this`
 // `spotify-this-song`
 // `movie-this`
 // `do-what-it-says`
-
-
+require("dotenv").config();
+var moment = require("moment");
+var axios = require("axios");
 
 var Spotify = require("node-spotify-api");
 var keys = require("./keys.js");
-//using the node-spotify-api package on npm
-//`node liri.js spotify-this-song '<song name here>'`
+
+
+
 
 var spotify = new Spotify(keys.spotify);
 const command = process.argv[2];
-
+const song = process.argv.slice(3).join(" ");
 if (command === "spotify-this") {
-  const song = process.argv.slice(3).join(" ");
-  spotify.search({ type: 'track', query: song, limit: 1 }, function(err, data) {
-    if (err) {
-      return console.log('Error occured: ' + err)
-    }
-    for(let i = 0; i < data.tracks.items.length; i++) {
-      const artistName = data.tracks.items[i].album.artists[0].name;
-      const songTitle = data.tracks.items[i].name;
-      const songUrl = data.tracks.items[i].preview_url;
-      const songAlbum = data.tracks.items[i].album[i];
-      // console.log(data.tracks)
-      //* Artist(s)
-      console.log("Band: " + artistName)
-      //* The song's name
-      console.log("Song: " + songTitle);
-      //* A preview link of the song from Spotify
-      console.log("Link to this song: " + songUrl)
-      //* The album that the song is from
-      console.log("Album: " + songAlbum)
-      console.log("\n------------------------------------")
-    }
-    //* If no song is provided then your program will default to "The Sign" by Ace of Base.
-    if (process.argv[3] === ""){
-      spotify.search({ type: 'track', query: 'The Sign', limit: 1 }, function(err, data){
-        if (err) {
-          return console.log('Error occured: ' + err)
-        }
-        for(let i = 0; i < data.tracks.items.length; i++) {
-          const artistName = data.tracks.items[i].album.artists[0].name;
-          const songTitle = data.tracks.items[i].name;
-          const songUrl = data.tracks.items[i].preview_url;
-          const songAlbum = data.tracks.items[i].album[i];
-          console.log("Band: " + artistName)
-          console.log("Song: " + songTitle);
-          console.log("Link to this song: " + songUrl)
-          console.log("Album: " + songAlbum)
-          console.log("\n------------------------------------")
-        }
-      })
+
+  spotify
+    .request('https://api.spotify.com/v1/search?q=track:' + song + '&type=track&limit=1')
+    .then(function (data) {
+      for (let i = 0; i < data.tracks.items.length; i++) {
+        const artistName = data.tracks.items[i].album.artists[0].name;
+        const songTitle = data.tracks.items[i].name;
+        const songUrl = data.tracks.items[i].preview_url;
+        const songAlbum = data.tracks.items[i].album.name;
+        // console.log(data.tracks)
+        //* Artist(s)
+        console.log("Band: " + artistName);
+        //* The song's name
+        console.log("Song: " + songTitle);
+        //* A preview link of the song from Spotify
+        console.log("Link to this song: " + songUrl);
+        //* The album that the song is from
+        console.log("Album: " + songAlbum);
+        console.log("\n------------------------------------");
+      }
       
-    }
-
-
-  });
-  
+    })
 }
+// * If no song is provided then your program will default to "The Sign" by Ace of Base.
+if (command === "spotify-this" + (process.argv[3] === " ")) {
+  spotify
+    .request('https://api.spotify.com/v1/search?q=track:The+Sign&type=track&limit=1')
+    .then(function (data) {
+      for (let i = 0; i < data.tracks.items.length; i++) {
+        const artistName = data.tracks.items[i].album.artists[0].name;
+        const songTitle = data.tracks.items[i].name;
+        const songUrl = data.tracks.items[i].preview_url;
+        const songAlbum = data.tracks.items[i].album.name;
+        // console.log(data.tracks)
+        //* Artist(s)
+        console.log("Band: " + artistName + "\nSong: " + songTitle)
+        //* The song's name
+        console.log( + "Song: " + songTitle);
+        //* A preview link of the song from Spotify
+        console.log( + "Link to this song: " + songUrl)
+        //* The album that the song is from
+        console.log("Album: " + songAlbum)
+        console.log("\n------------------------------------")
+      }
 
-
-//using the axios package on npm
-
-// `node liri.js movie-this '<movie name here>'`
+    })
+}
 
 var input = process.argv.slice(3).join(" ");
 axios.get("http://www.omdbapi.com/?t=" + input + "&y=&plot=short&tomatoes=true&apikey=trilogy").then(
   function (response) {
-
     if (process.argv[2] === "movie-this") {
-      //          Title of the movie.
-      console.log(response.data.Title);
-      //          Year the movie came out.
-      console.log( + response.data.Year);
-      //          IMDB Rating of the movie.
-      console.log( + response.data.imdbRating);
-      //          Rotten Tomatoes Rating of the movie.
-      console.log( + response.data.tomatoRating);
-      //          Country where the movie was produced.
-      console.log( + response.data.Country);
-      //          Language of the movie.
-      console.log( + response.data.Language);
-      //          Plot of the movie.
-      console.log( + response.data.Plot);
-      //          Actors in the movie.
-      console.log( + response.data.Actors);
+      console.log("Title: " + response.data.Title + "\nYear: " + response.data.Year + "\nIMDB Rating: " + response.data.imdbRating +
+        "\nRotten Tomatoes Rating: " + response.data.tomatoRating + "\nCountry: " + response.data.Country + "\nLanguage: " + response.data.Language +
+        "\nPlot: " + response.data.Plot + "\nCast: " + response.data.Actors);
     }
-    //  If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-    // else {
-    //     axios.get("http://www.omdbapi.com/?t=Mr+Nobody&y=&plot=short&tomatoes=true&apikey=trilogy").then(
-    //         function (response) {
-    //             console.log(response.data.Title);
-    //             console.log(response.data.Year);
-    //             console.log(response.data.imdbRating);
-    //             console.log(response.data.tomatoRating);
-    //             console.log(response.data.Country);
-    //             console.log(response.data.Language);
-    //             console.log(response.data.Plot);
-    //             console.log(response.data.Actors);
-    //         })
-    // }
+
+  })
+
+  //trying to get the default answer when no movie is looked up
+  //changed the query to a request of the specific title
+  //changed the argument to include an empty string
+axios.get("https://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy").then(
+  function (response) {
+    if (process.argv[2] === "movie-this" + (process.argv[3] === " ")) {
+      console.log("Title: " + response.data.Title + "\nYear: " + response.data.Year + "\nIMDB Rating: " + response.data.imdbRating +
+        "\nRotten Tomatoes Rating: " + response.data.tomatoRating + "\nCountry: " + response.data.Country + "\nLanguage: " + response.data.Language +
+        "\nPlot: " + response.data.Plot + "\nCast: " + response.data.Actors);
+    }
+
   })
 
 

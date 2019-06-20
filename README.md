0587eae3c3b52e087e5023e8b1a04ca24d4d6fee
Michael Barachkov <mike.barachkov@outlook.com>
2019-06-19 23:16:41 -0700

    added readme and more debugging to the no keyword responses

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# liri-node-app
+
+
+
+# LIRI Node App
+
+## [LIRI APP EXAMPLE VIDEO](https://drive.google.com/file/d/13W_zt65bpLFTmAboyjPL44DGvhj-ZOz4/view)
+
+*Command List
+    
+    this will give you a response of the soonest concert from your Artist
+    *node liri.js concert-this "Artist"
+
+    this will give you all of the information about the song you enter
+    *node liri.js spotify-this "Song"
+
+    this will give you some information about the movie you enter
+    *node liri.js movie-this "Movie"
+
+# About this app
+
+**Technologies**
+    
+    *Javascript
+    *node js
+    *npm packages (moment, axios, node-spotify-api, file system(fs))
+
+# Future for this app
+
+    *add responses for movie-this and spotify-this when no keyword is entered
+    *change response of do-what-it-says to a string then a search
+
--- liri.js
+++ liri.js
@@ -5,7 +5,7 @@
 require("dotenv").config();
 var moment = require("moment");
 var axios = require("axios");
-
+var fs = require("fs");
 var Spotify = require("node-spotify-api");
 var keys = require("./keys.js");
 
@@ -13,8 +13,8 @@ const input = process.argv.slice(3).join(" ");
 axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp").then(
   function (response) {
     if (process.argv[2] === "concert-this") {
-      console.log("Venue: "+ response.data[0].venue.name + "\nCity: " + response.data[0].venue.city + 
-      "\nDate: " + moment(response.data[0].daytime).format("MM//DD/YYYY"));
+      console.log("Venue: " + response.data[0].venue.name + "\nCity: " + response.data[0].venue.city +
+        "\nDate: " + moment(response.data[0].daytime).format("MM//DD/YYYY") + "\n------------------------------------");
     }
 
   })
@@ -32,38 +32,52 @@ if (command === "spotify-this") {
         const songTitle = data.tracks.items[i].name;
         const songUrl = data.tracks.items[i].preview_url;
         const songAlbum = data.tracks.items[i].album.name;
-        // console.log(data.tracks)
-        //* Artist(s)
-        console.log("Band: " + artistName);
-        //* The song's name
-        console.log("Song: " + songTitle);
-        //* A preview link of the song from Spotify
-        console.log("Link to this song: " + songUrl);
-        //* The album that the song is from
-        console.log("Album: " + songAlbum);
-        console.log("\n------------------------------------");
-      }
-      
-    })
-}
-// * If no song is provided then your program will default to "The Sign" by Ace of Base.
-if (command === "spotify-this" + (process.argv[3] === " ")) {
-  spotify
-    .request('https://api.spotify.com/v1/search?q=track:The+Sign&type=track&limit=1')
-    .then(function (data) {
-      for (let i = 0; i < data.tracks.items.length; i++) {
-        const artistName = data.tracks.items[i].album.artists[0].name;
-        const songTitle = data.tracks.items[i].name;
-        const songUrl = data.tracks.items[i].preview_url;
-        const songAlbum = data.tracks.items[i].album.name;
 
-        console.log("Band: " + artistName + "\nSong: " + songTitle + "\nLink to this song: " + 
-        songUrl + "\nAlbum: " + songAlbum + "\n------------------------------------")
-        
+        console.log("Band: " + artistName + "\nSong: " + songTitle + "\nLink to this song: " +
+          songUrl + "\nAlbum: " + songAlbum + "\n------------------------------------")
       }
 
     })
+  if (input === "the sign") {
+    spotify
+      .request('https://api.spotify.com/v1/search?q=track:The+Sign&type=track&limit=1')
+      .then(function (data) {
+        for (let i = 0; i < data.tracks.items.length; i++) {
+          const artistName = data.tracks.items[i].album.artists[0].name;
+          const songTitle = data.tracks.items[i].name;
+          const songUrl = data.tracks.items[i].preview_url;
+          const songAlbum = data.tracks.items[i].album.name;
+
+          console.log("Band: " + artistName + "\nSong: " + songTitle + "\nLink to this song: " +
+            songUrl + "\nAlbum: " + songAlbum + "\n------------------------------------")
+
+        }
+
+      })
+  }
+
 }
+
+//trying to get the default answer when no song is looked up
+//changed the query to a request of the specific title
+//changed the argument to include an empty string
+// if (input === "the sign") {
+//   spotify
+//     .request('https://api.spotify.com/v1/search?q=track:The+Sign&type=track&limit=1')
+//     .then(function (data) {
+//       for (let i = 0; i < data.tracks.items.length; i++) {
+//         const artistName = data.tracks.items[i].album.artists[0].name;
+//         const songTitle = data.tracks.items[i].name;
+//         const songUrl = data.tracks.items[i].preview_url;
+//         const songAlbum = data.tracks.items[i].album.name;
+
+//         console.log("Band: " + artistName + "\nSong: " + songTitle + "\nLink to this song: " +
+//           songUrl + "\nAlbum: " + songAlbum + "\n------------------------------------")
+
+//       }
+
+//     })
+// }
 
 
 axios.get("http://www.omdbapi.com/?t=" + input + "&y=&plot=short&tomatoes=true&apikey=trilogy").then(
@@ -71,31 +85,26 @@ axios.get("http://www.omdbapi.com/?t=" + input + "&y=&plot=short&tomatoes=true&a
     if (process.argv[2] === "movie-this") {
       console.log("Title: " + response.data.Title + "\nYear: " + response.data.Year + "\nIMDB Rating: " + response.data.imdbRating +
         "\nRotten Tomatoes Rating: " + response.data.tomatoRating + "\nCountry: " + response.data.Country + "\nLanguage: " + response.data.Language +
-        "\nPlot: " + response.data.Plot + "\nCast: " + response.data.Actors);
+        "\nPlot: " + response.data.Plot + "\nCast: " + response.data.Actors + "\n------------------------------------");
     }
 
-  })
-
-  //trying to get the default answer when no movie is looked up
-  //changed the query to a request of the specific title
-  //changed the argument to include an empty string
-axios.get("https://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy").then(
-  function (response) {
-    if (process.argv[2] === "movie-this" + (process.argv[3] === " ")) {
-      console.log("Title: " + response.data.Title + "\nYear: " + response.data.Year + "\nIMDB Rating: " + response.data.imdbRating +
-        "\nRotten Tomatoes Rating: " + response.data.tomatoRating + "\nCountry: " + response.data.Country + "\nLanguage: " + response.data.Language +
-        "\nPlot: " + response.data.Plot + "\nCast: " + response.data.Actors);
-    }
-
+    //trying to get the default answer when no movie is looked up
+    // if (input === "mr nobody") {
+    //   console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/" + "\nIt's on Netflix!");
+    // }
   })
 
 
-
-//using the fs node package
+//using file system (fs) node
 // `node liri.js do-what-it-says`
-
-//    * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-
-//      * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
-//      * Edit the text in random.txt to test out the feature for movie-this and concert-this.
-//_______________________________________________________________
+if (command === "do-what-it-says") {
+  fs.readFile("random.txt", "utf8", function (error, data) {
+    if (error) {
+      return console.log(error);
+    }
+    // still need to change dataArr into a string
+    //and turn that string into a search
+    var dataArr = data.split(",");
+    console.log(dataArr);
+  });
+}
